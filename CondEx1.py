7d3f028472a659027ffb00b336cc5d249b876828
jamesryandc <jocadano@up.edu.ph>
2020-03-11 16:43:47 +0800

    Conditionals 1

---
+++ CondEx1.py
@@ -0,0 +1,11 @@
+print ("Pay Calculator")
+
+hrs = float(input("Enter Hours: "))
+rate = float(input("Enter Rate: "))
+
+if hrs > 40 :
+    hrs = hrs - 40.0
+    pay = (40*rate) + (hrs*rate*1.5)
+else:
+    pay = rate*hrs
+print("Pay: ", pay)
